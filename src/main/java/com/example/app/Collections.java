9405849bc3fba55281d8d31a30db1ea9533edbac
Alexander <garncev@gmail.com>
2021-02-08 13:11:49 +0300

    Various method of multiset

--- src/main/java/com/example/app/Collections.java
+++ src/main/java/com/example/app/Collections.java
@@ -24,6 +24,30 @@ public class Collections {
     public static void main(String[] args) {
         out(traditionalApproach());
         usingGuavaMutliset();
+        variousMethodsOfMultiSet();
+        multiMapUsage();
+    }
+
+    private static void multiMapUsage() {
+    }
+
+    private static void variousMethodsOfMultiSet() {
+        TreeMultiset<String> multiset = TreeMultiset.create();
+        multiset.add("a");
+        multiset.add("a");
+        multiset.add("b");
+        System.out.println(multiset.size());
+        // For a HashMultiset, count is O(1), for a TreeMultiset, count is O(log n), etc.
+        System.out.println(multiset.count("a"));
+        System.out.println(multiset.count("b"));
+        System.out.println(multiset.count("c"));
+        multiset.add("c", 5);
+        multiset.remove("c", 2);
+        for (String s : multiset.elementSet()) {
+            System.out.printf("%s: %d%n", s, multiset.count(s));
+        }
+        multiset.setCount("c", 1);
+        System.out.println(multiset.size());
     }
 
     private static void usingGuavaMutliset() {
